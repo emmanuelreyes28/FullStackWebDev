961bdeb37f686e660e45deec019c7452c7b907b1
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2022-07-12 23:18:12 -0700

    Finished Dice Challenge

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -1,9 +1,20 @@
-function getRandomRoll() {
-  return Math.floor(Math.random() * 6) + 1;
-}
+// function getRandomRoll() {
+//   return Math.floor(Math.random() * 6) + 1;
+// }
 
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var image1 = "images/dice" + randomNumber1 + ".png";
+var leftImage = "images/dice" + randomNumber1 + ".png";
+var rightImage = "images/dice" + randomNumber2 + ".png";
 
-document.querySelector(".dice .img1").setAttribute("src", image1);
+document.querySelector(".dice .img1").setAttribute("src", leftImage);
+document.querySelector(".dice .img2").setAttribute("src", rightImage);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").textContent = "🚩Player 1 Wins";
+} else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").textContent = "Player 2 Wins🚩";
+} else {
+  document.querySelector("h1").textContent = "Draw";
+}
