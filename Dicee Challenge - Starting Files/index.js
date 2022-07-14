f0012e8ee5c1b34dd156dd16be48c05b7d5bb5c5
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2022-07-13 23:06:34 -0700

    Updated index.js
    Wrote different approaches to solution

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -2,19 +2,44 @@
 //   return Math.floor(Math.random() * 6) + 1;
 // }
 
+// var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+// var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+// var leftImage = "images/dice" + randomNumber1 + ".png";
+// var rightImage = "images/dice" + randomNumber2 + ".png";
+
+// document.querySelector(".dice .img1").setAttribute("src", leftImage);
+// document.querySelector(".dice .img2").setAttribute("src", rightImage);
+
+// if (randomNumber1 > randomNumber2) {
+//   document.querySelector("h1").textContent = "🚩Player 1 Wins";
+// } else if (randomNumber1 < randomNumber2) {
+//   document.querySelector("h1").textContent = "Player 2 Wins🚩";
+// } else {
+//   document.querySelector("h1").textContent = "Draw";
+// }
+
+//different way of approaching solution
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "/dice" + randomNumber1 + ".png";
+
+var randomImageSource = "images" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var leftImage = "images/dice" + randomNumber1 + ".png";
-var rightImage = "images/dice" + randomNumber2 + ".png";
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
-document.querySelector(".dice .img1").setAttribute("src", leftImage);
-document.querySelector(".dice .img2").setAttribute("src", rightImage);
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").textContent = "🚩Player 1 Wins";
-} else if (randomNumber1 < randomNumber2) {
-  document.querySelector("h1").textContent = "Player 2 Wins🚩";
+  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins";
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins 🚩";
 } else {
-  document.querySelector("h1").textContent = "Draw";
+  document.querySelector("h1").innerHTML("Draw!");
 }
