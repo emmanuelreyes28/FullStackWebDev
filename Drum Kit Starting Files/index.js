25d3330f24a9b1f31fa313b414c5466bda9a604c
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2022-07-14 23:43:41 -0700

    Updated drum kit page functionality

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -2,9 +2,13 @@ var button = document.querySelectorAll(".drum");
 // .addEventListener("click", clickMe);
 
 function clickMe() {
-  alert("I got clicked");
+  //console.log(this.color);
+  this.style.color = "white";
 }
 
 for (var i = 0; i < button.length; i++) {
   button[i].addEventListener("click", clickMe);
 }
+
+// var tom1 = new Audio("sounds/tom-1.mp3");
+// tom1.play();
