eca6ab7c6e632352e411ad313dd47ff5b8ddf140
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2022-07-15 23:02:58 -0700

    Added keyboard event listeners

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,14 +1,65 @@
-var button = document.querySelectorAll(".drum");
-// .addEventListener("click", clickMe);
+//Detecting button press
 
-function clickMe() {
-  //console.log(this.color);
-  this.style.color = "white";
-}
+let button = document.querySelectorAll(".drum");
 
-for (var i = 0; i < button.length; i++) {
+for (let i = 0; i < button.length; i++) {
   button[i].addEventListener("click", clickMe);
 }
 
-// var tom1 = new Audio("sounds/tom-1.mp3");
+function clickMe() {
+  let buttonInnerHTML = this.innerHTML;
+  makeSound(buttonInnerHTML);
+}
+
+//Detecting keyboard press
+
+document.addEventListener("keydown", function (event) {
+  // listen for any and all key presses so no need to create for loop
+  // key.event gives us the key that was pressed
+  makeSound(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      let tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "a":
+      let tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      let tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "d":
+      let tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    case "j":
+      let snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "k":
+      let crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "l":
+      let kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    default:
+      console.log(buttonInnerHTML);
+  }
+}
+
+// let tom1 = new Audio("sounds/tom-1.mp3");
 // tom1.play();
