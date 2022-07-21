00095c5e1c8786665a3d8e3871f27ebd8401b5b7
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2022-07-20 23:09:19 -0700

    More jQuery

--- jQuery/index.js
+++ jQuery/index.js
@@ -2,3 +2,42 @@
 
 //adds css class big title and maring 50 to h1
 $("h1").addClass("big-title margin-50");
+
+//manipulate text with jQuery
+$("h1").text("Bye");
+
+$("button").html("<em>Click me</em>");
+
+//get attribute values
+console.log($("img").attr("src"));
+
+//set attribute values
+$("a").attr("href", "https://www.yahoo.com");
+
+//Adding event listeners with jQuery
+
+//if h1 clicked change color to purple
+$("h1").click(function () {
+  $("h1").css("color", "purple");
+});
+
+//select all buttons in page and if clicked changes the h1 color to purple
+$("button").click(function () {
+  $("h1").css("color", "purple");
+});
+
+//checks if keypress has occured within input and prints key to console
+$("input").keypress(function (event) {
+  console.log(event.key);
+});
+
+//checks for any keypressed within the whole page and
+//changes the h1 text to the key that was pressed
+$("body").keypress(function (event) {
+  $("h1").text(event.key);
+});
+
+//more flexible way of adding event listener by using on method
+$("h1").on("mouseover", function () {
+  $("h1").css("color", "blue");
+});
