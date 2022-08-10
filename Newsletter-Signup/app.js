3208c74eb505ac62788da9a3de355e73a98ff9c7
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2022-08-10 12:40:10 -0700

    Added success and failure pages
    Handle response status codes with success and failure pages

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -44,6 +44,13 @@ app.post("/", function (req, res) {
 
   //make post request to mailchimp using user data entered to sign up page
   const request = https.request(url, options, function (response) {
+    //check if response was successful (200)
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+
     response.on("data", function (data) {
       console.log(JSON.parse(data));
     });
@@ -54,7 +61,12 @@ app.post("/", function (req, res) {
   request.end();
 });
 
-app.listen(3000, function () {
+//redirect user to root route from failure page when try again button pressed
+app.post("/failure", function (req, res) {
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server is running on port 3000");
 });
 
