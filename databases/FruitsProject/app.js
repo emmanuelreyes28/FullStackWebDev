2e58f71c608262476ade7378af71e8b3fa6aa05f
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2022-09-06 22:54:24 -0700

    Implement mongoose

--- databases/FruitsProject/app.js
+++ databases/FruitsProject/app.js
@@ -1,50 +1,44 @@
-const { MongoClient } = require("mongodb");
+const mongoose = require('mongoose');
 
-// Replace the uri string with your connection string.
+//mongoose.connect("mongodb://localhost:27017/fruitsDB", { useNewUrlParser: true, useUnifiedTopology: true });
+run().catch(err => console.log(err));
 
-const uri = "mongodb://localhost:27017";
-
-const client = new MongoClient(uri, {useUnifiedTopology: true});
-
-const dbName = "fruitsDB";
-
-async function run() {
-  try {
-    await client.connect(function(){
-        console.log("connected successfully to server");
-    })
-    const database = client.db(dbName);
-    //get the documents collection
-    const collection = database.collection('fruits');
-
-    //documents to be insterted in fruitsDB
-    const docs = [
-        {
-          name: 'Apple',
-          score: 8,
-          review: "Great fruit"
-        },
-        {
-          name: "Orange",
-          score: 6,
-          review: "Kinda sour"
-        },
-        {
-          name: "Bananna",
-          score: 9,
-          review: "Great stuff!"
-        }
-    ];
-
-    const options = {ordered: true};
-
-    //insert some documents
-    const result = await collection.insertMany(docs, options);
-
-    console.log("documents were inserted");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+async function run(){
+    // connect to mongo server and specify db that we want to create or connect to 
+    await mongoose.connect("mongodb://localhost:27017/personDB");
 }
-run().catch(console.dir);
+
+// create scheman with properties
+const fruitSchema = new mongoose.Schema({
+    name: String,
+    rating: Number,
+    review: String
+});
+
+// turn schema into a model
+// mongoose will turn "Fruit" into fruits 
+const Fruit = mongoose.model("Fruit", fruitSchema);
+
+// create a new fruit document
+const fruit = new Fruit({
+    name: "Apple",
+    rating: 7,
+    review: "pretty solid as a fruit."
+});
+
+// save fruit document in fruitDB
+//fruit.save()
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    age: Number
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+    name: "John",
+    age: 37
+});
+
+person.save();
