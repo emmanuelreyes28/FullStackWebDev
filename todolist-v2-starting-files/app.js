9df680fd3c8789bec4bbd709c85389439f10d02b
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2022-09-25 16:18:35 -0700

    Connect to mongodb atlas

--- todolist-v2-starting-files/app.js
+++ todolist-v2-starting-files/app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 //connect to mongoose db
-mongoose.connect("mongodb://localhost:27017/todolistDB");
+mongoose.connect("mongodb+srv://admin-emmanuel:Test123@cluster0.mnfnjpg.mongodb.net/todolistDB");
 
 //create mongoose schema
 const itemSchema = {
