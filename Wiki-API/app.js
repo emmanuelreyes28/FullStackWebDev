c1db1565cc32d92bc53291a723eb7f4e1f3e74b9
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2022-09-29 23:51:53 -0700

    Add get articles route

--- Wiki-API/app.js
+++ Wiki-API/app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 //store our static files in public
 app.use(express.static("public"));
 
-mongoose.connect('mongodb://localhost:27017/wikiDB', {userNewUrlParser: true});
+mongoose.connect('mongodb://localhost:27017/wikiDB', {useNewUrlParser: true});
 
 const articleSchema = {
     title: String,
@@ -20,6 +20,17 @@ const articleSchema = {
 }
 
 const Article = mongoose.model("Article", articleSchema);
+
+//GET /articles route fetches all the articles
+app.get("/articles", function(req, res){
+    Article.find(function(err, foundArticles){
+        if(!err){
+            res.send(foundArticles);
+        } else{
+            res.send(err);
+        }
+    });
+}); 
 
 app.listen(3000, function(){
     console.log("Server started on port 3000");
