69f964c96db43345880dedec7d373020814fbd69
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2022-09-30 23:34:13 -0700

    Create post route for new article

--- Wiki-API/app.js
+++ Wiki-API/app.js
@@ -21,16 +21,41 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
-//GET /articles route fetches all the articles
+//GET /articles route fetches all the articles from wikiDB's Article model 
 app.get("/articles", function(req, res){
+    //find function here does not take a condition bc we want to grab ALL articles 
     Article.find(function(err, foundArticles){
         if(!err){
+            //if no error respond with articles found 
             res.send(foundArticles);
         } else{
+            //respond with err found
             res.send(err);
         }
     });
 }); 
+
+//use postman to send data with post without having to build a front end
+//post new data requested to wikiDB with title and content key/values and save document to db
+app.post("/articles", function(req, res){
+
+    //create new article document using Article model
+    const newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content
+    });
+
+    //save if there were no errors encountered 
+    newArticle.save(function(err){
+        if(!err){
+            //respond with success message
+            res.send("Successfully added a new article");
+        } else{
+            //respond with errr encountered
+            res.send(err);
+        }
+    });
+});
 
 app.listen(3000, function(){
     console.log("Server started on port 3000");
